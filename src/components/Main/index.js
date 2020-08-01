57396a3d7acc3bbda2950ae2958a21fdd8a969f3
Everton Trajano <evertontrajoli@gmail.com>
2020-07-31 22:23:07 -0300

    MVP correção

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import BannerMain from '../BannerMain'
 import Carousel from '../Carousel'
 import data from '../../data/dados_iniciais.json'
+import { url,videoTitle, videoDescription } from '../../data/bannerMain.json'
 import { Main } from './styles'
 
 
 export default function () {
     return (
         <Main>
-            <BannerMain url='https://www.youtube.com/watch?v=UOK7nS2E9xM' videoTitle="Teste" videoDescription="Texte" />
+            <BannerMain url={url} videoTitle={videoTitle} videoDescription={videoDescription} />
             {
                 data['categorias'].map(
                     categoria =>
