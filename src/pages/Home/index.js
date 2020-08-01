dfebff66058a75cdecc2569b04691de63f836b09
Everton Trajano <evertontrajoli@gmail.com>
2020-08-01 01:55:36 -0300

    Adição de rotas

---
+++ src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Menu from '../../components/Menu'
+import Main from '../../components/Main'
+/*
+videoTitle,
+  videoDescription,
+  url,
+*/
+
+
+export default function () {
+    return (
+        <div>
+            <Menu />
+            <Main />
+        </div>
+    )
+}
